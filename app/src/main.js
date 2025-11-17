6b8c8fdc29179bb522252b6c3f158454370fd686
selinal72 <selinal72@nycstudents.net>
2025-11-17 14:01:57 -0500

    werk

--- app/src/main.js
+++ app/src/main.js
@@ -1,6 +1,33 @@
 import "./style.css";
 import { setupCounter } from "./counter.js";
 
+let songs = [
+  {
+    title: "Mr. Incorrect",
+    artists: "Malcolm Todd",
+    genres: ["Alternative", "Indie", "R&B"],
+    cover: "sweetboy.jpg",
+  },
+  {
+    title: "Magic Johnson",
+    artists: "ian",
+    genres: ["Hip Hop", "Rap"],
+    cover: "valedictorian.jpg",
+  },
+  {
+    title: "Bags",
+    artists: "Clairo",
+    genres: ["Alternative", "Indie", "Bedroom"],
+    cover: "immunity.jpg",
+  },
+  {
+    title: "Bleed (feat. Omar Apollo)",
+    artists: "Malcolm Todd",
+    genres: ["Alternative", "Indie", "Bedroom"],
+    cover: "immunity.jpg",
+  },
+];
+
 document.querySelector("#app").innerHTML = `
   <div>
     <a href="https://open.spotify.com/playlist/7lURg77utY7QX9xasBrUMs?si=flNQ_9c6RGmG705Rlp32Zg" target="_blank">
@@ -12,9 +39,12 @@ document.querySelector("#app").innerHTML = `
       <button id="counter" type="button"></button>
     </div>
     <p class="read-the-docs">
-      Click on the Vite logo to learn more
+      Click on the Spotify logo to view a playlist reccomendation
     </p>
   </div>
 `;
+
+songs.forEach((song) =>
+)
 
 setupCounter(document.querySelector("#counter"));
